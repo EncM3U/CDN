a83b642bda7eabf15f53f79219b9e9d57cad774f
MoChanBW <bryanw@vip.qq.com>
2020-07-09 12:20:47 +0800

    update

--- aplayerControllerGen.py
+++ aplayerControllerGen.py
@@ -25,20 +25,21 @@ def musicConverter(allMusicInDict):
                 allMusicInDict[keys]["Name"]+'"'+' -b:a 280k "' + \
                 allMusicInDict[keys]["Name"].replace(".mp3", "")+'(280k).mp3"'
             print("正在以280k码率重编码...", keys)
-            #print(command)
+            # print(command)
             subp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE, encoding="utf-8")
             time.sleep(8)  # 等ffmpeg运行
             if subp.poll() == 0:
-                print("subp.communicate()[0]",subp.communicate()[0])
-                print("subp.poll()=0: "+allMusicInDict[keys]["Name"].replace(".mp3", "")+'(280k).mp3')
+                print("subp.communicate()[0]", subp.communicate()[0])
+                print(
+                    "subp.poll()=0: "+allMusicInDict[keys]["Name"].replace(".mp3", "")+'(280k).mp3')
                 data = {
                     'name': keys,
                     'artist': allMusicInDict[keys]["Artist"],
                     'url': base+allMusicInDict[keys]["Name"].replace(".mp3", "")+'(280k).mp3',
-                    'cover': base+allMusicInDict[keys]["Name"].replace(".mp3", ".jpg"),
-                    'lrc': base+allMusicInDict[keys]["Name"].replace(".mp3", ".lrc"),
-                    'album':allMusicInDict[keys]["Album"],
+                    'cover': "",
+                    'lrc': "",
+                    'album': allMusicInDict[keys]["Album"],
                 }
                 strs = strs + str(data) + ","
                 k += 1
@@ -52,23 +53,22 @@ def musicConverter(allMusicInDict):
                     'name': keys,
                     'artist': allMusicInDict[keys]["Artist"],
                     'url': base+allMusicInDict[keys]["Name"].replace(".mp3", "")+'(280k).mp3',
-                    'cover': base+allMusicInDict[keys]["Name"].replace(".mp3", ".jpg"),
-                    'lrc': base+allMusicInDict[keys]["Name"].replace(".mp3", ".lrc"),
-                    'album':allMusicInDict[keys]["Album"],
+                    'cover': "",
+                    'lrc': "",
+                    'album': allMusicInDict[keys]["Album"],
                 }
                 strs = strs + str(data) + ","
                 k += 1
-                #print(allMusicInDict[keys]["Name"])
-                #print(data)
-
+                # print(allMusicInDict[keys]["Name"])
+                # print(data)
         else:
             data = {
                 'name': keys,
                 'artist': allMusicInDict[keys]["Artist"],
                 'url': base+allMusicInDict[keys]["Name"],
-                'cover': base+allMusicInDict[keys]["Name"].replace(".mp3", ".jpg"),
-                'lrc': base+allMusicInDict[keys]["Name"].replace(".mp3", ".lrc"),
-                'album':allMusicInDict[keys]["Album"],
+                'cover': "",
+                'lrc': "",
+                'album': allMusicInDict[keys]["Album"],
             }
             strs = strs + str(data) + ","
             k += 1
@@ -86,9 +86,11 @@ def musicConverter(allMusicInDict):
             volume: 0.7,
             mutex: true,
             listFolded: false,
-            listMaxHeight: 90,
             audio: ["""+strs+"""]
-            });"""
+            lrcType: 1,
+            });
+            console.log('normal')
+            """
         f = open('aplayerMainController.js', mode='w', encoding='utf-8')
         f.write(js)
         f.close()
@@ -96,6 +98,74 @@ def musicConverter(allMusicInDict):
         js = ""
         jsfixed = jsfixed.replace("autoplay: true", "autoplay: false")
         jsfixed = jsfixed.replace("mini: false", "fixed: true")
+        jsfixed = jsfixed.replace("console.log('normal')", """var theLIST = ap.list.audios;
+        ap.list["index"];
+        console.log(theLIST);
+        var ShowList = 0;
+        ap.on("listshow", function () {
+            ShowList = 1;
+            document.getElementById("aplayer").style = "top:0;position:fixed;overflow-y:auto;";
+        });
+        ap.on("listhide", function () {
+            ShowList = 0;
+            document.getElementById("aplayer").style = "top:1;position:fixed;overflow-y:auto;";
+        });
+        var miniswitcherON = 0;
+        document.getElementsByClassName("aplayer-miniswitcher")[0].onclick =
+            function () {
+                if (miniswitcherON == 0) {
+                    miniswitcherON = 1;; //现状态
+                    if (ShowList) {
+                        window.setTimeout(function () {
+                            document.getElementById("aplayer").style =
+                                "top:0;position:fixed;overflow-y:auto;";
+                        }, 300);
+                    } else {
+                        document.getElementById("aplayer").style = "top:1;position:fixed;overflow-y:auto;";
+                    }
+                } else {
+                    miniswitcherON = 0; //现状态
+                    document.getElementById("aplayer").style = "top:1;position:fixed;overflow-y:auto;";
+                }
+            };
+        ap.on("loadstart", function () {
+            if (ap.list.audios[ap.list["index"]].lrc == "" && (ap.list.audios[ap.list["index"]].cover == "" ||
+                    ap.list.audios[ap.list["index"]].cover == undefined)) {
+                console.log("undefine yes")
+                var Name = theLIST[ap.list["index"]].name;
+                var Album = theLIST[ap.list["index"]].album;
+                var Artist = theLIST[ap.list["index"]].artist;
+                axios.get('https://api.mochanbw.cn/QMapi/search', {
+                        params: {
+                            key: Artist + " " + Name + " " + Album
+                        }
+                    })
+                    .then(function (response) {
+                        var theSongmid = response.data.data.list[0].songmid;
+                        var theAlbummid = response.data.data.list[0].albummid;
+                        theLIST[ap.list["index"]].cover =
+                            "https://y.gtimg.cn/music/photo_new/T002R300x300M000" + theAlbummid +
+                            ".jpg";
+                        axios.get('https://api.mochanbw.cn/QMapi/lyric', {
+                                params: {
+                                    songmid: theSongmid
+                                }
+                            })
+                            .then(function (response) {
+                                theLIST[ap.list["index"]].lrc = response.data.data.lyric;
+                                let ID = ap.list["index"];
+                                ap.list.clear();
+                                ap.list.add(theLIST);
+                                ap.list.switch(ID);
+
+                            });
+
+                    });
+
+
+            }
+        });""")
+
         fixed = open('aplayerMainFixedController.js',
                      mode='w', encoding='utf-8')
         fixed.write(jsfixed)
@@ -133,7 +203,7 @@ def lrcEncodingConvertToUTF8(dirs):
                               matchObj.group(), ',try decoding it manually...')
                         enc = ""
 
-                        for encmanually in ["gbk","gb18030","ansi", "utf-16", "gb2312","utf-8", "euc-jp"]:
+                        for encmanually in ["gbk", "gb18030", "ansi", "utf-16", "gb2312", "utf-8", "euc-jp"]:
                             try:
                                 print("    Try decoding the lrc file by",
                                       encmanually, "...")
@@ -213,13 +283,13 @@ def probe(dirs):
                                      matchObj, re.M | re.I)
                     if KeObj:
                         subdict = {}
-                        subdict["Artist"] = KeObj.group(1).replace("_","")
+                        subdict["Artist"] = KeObj.group(1).replace("_", "")
                         subdict["Name"] = KeObj.group()
                         #print("============", subdict["Name"])
                         subdict["Slice"] = False
                         subdict["Album"] = ""
                         returnDict = {}
-                        returnDict[KeObj.group(2).replace("_","")] = subdict
+                        returnDict[KeObj.group(2).replace("_", "")] = subdict
                         #print("keObj returnDict: ", returnDict)
                         return returnDict
                 else:
@@ -227,13 +297,13 @@ def probe(dirs):
                                      matchObj, re.M | re.I)
                     if KeObj:
                         subdict = {}
-                        subdict["Artist"] = KeObj.group(1).replace("_","")
+                        subdict["Artist"] = KeObj.group(1).replace("_", "")
                         subdict["Name"] = KeObj.group()
                         # print("============",KeObj.group(0))
                         subdict["Slice"] = True
                         subdict["Album"] = ""
                         returnDict = {}
-                        returnDict[KeObj.group(2).replace("_","")] = subdict
+                        returnDict[KeObj.group(2).replace("_", "")] = subdict
                         #print("returnDict: ",returnDict)
                         return returnDict
 
